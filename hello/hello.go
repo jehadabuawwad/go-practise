7ec04d4ecca3ae26443ed073d1a42392f353ed77
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-08-21 15:36:11 +0300

    feat: generate random greetings

--- hello/hello.go
+++ hello/hello.go
@@ -2,21 +2,27 @@ package main
 
 import (
     "fmt"
-	"log"
+    "log"
+
     "example/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-	log.SetPrefix("greetings : ")
-	log.SetFlags(0)
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
 
+    // Request a greeting message.
+    message, err := greetings.Hello("Gladys")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
 
-    message, err := greetings.Hello("")
-    
-	if err != nil{
-		log.Fatal(err)
-	}
-	
-	fmt.Println(message)
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(message)
 }
