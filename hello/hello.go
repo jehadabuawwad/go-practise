d486ddfc9b6f319a32b7fc971f4be21382986662
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-08-21 15:31:01 +0300

    feat: error handling

---
+++ hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+	"log"
+    "example/greetings"
+)
+
+func main() {
+    // Get a greeting message and print it.
+	log.SetPrefix("greetings : ")
+	log.SetFlags(0)
+
+
+    message, err := greetings.Hello("")
+    
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+	fmt.Println(message)
+}
